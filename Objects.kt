90ccc97fbdcb33df2f22069a24ea63ed646ef7d5
Anand Nishad <90544787+anandrex5@users.noreply.github.com>
2022-07-04 02:14:55 +0530

    Add files via upload
    1) How we create objects in Kotlin 
    2) How we assign values in Kotlin 
    3) How do we print assign values without Concatenation (i.e, using ${ value} )

---
+++ Objects.kt
@@ -0,0 +1,22 @@
+fun main()
+{
+    println("Hello Aliens")
+    //crate an object
+    var tittu = Alien();
+    //assign the value
+    tittu.name = "Harsh"
+
+
+    //concatenation
+    println("My name is :" +tittu.name)
+
+    //or same thing but without concatenation
+
+    println("My name is : ${tittu.name}")
+
+}
+
+
+
+
+
